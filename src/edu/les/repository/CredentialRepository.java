0d5345a5295236dd5e3967bcac3bb8a62c9b6f6b
Vinicius Escame <v.escame@gmail.com>
2019-10-30 00:15:24 -0300

    added query that search for an specific register, not all of them

--- src/edu/les/repository/CredentialRepository.java
+++ src/edu/les/repository/CredentialRepository.java
@@ -1,7 +1,9 @@
 package edu.les.repository;
 
+import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 //import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +11,12 @@ import edu.les.entity.CredentialEntity;
 
 @Repository
 public interface CredentialRepository extends CrudRepository<CredentialEntity, Integer> {
-//	@Query("FROM credential t1 inner join hotel_user t2 on (t1.email = t2.email) "
-//			+ "WHERE t2.email = :v_email AND t1.password = :v_password")
-//	public CredentialEntity findLoginAndPassword(@Param("v_email") String username,
-//			@Param("v_password") String password);
+	/* exemplo
+	 * select t1.email, t1.password FROM credential t1 inner join hotel_user t2 on (t1.email = t2.email) WHERE t2.email =
+	 * 'v.escame@outlook.com' AND t1.password = 'viny0102'
+	 */
+	@Query("FROM credential t1 inner join hotel_user t2 on (t1.email = t2.email) "
+			+ "WHERE t2.email = :v_email AND t1.password = :v_password")
+	public CredentialEntity findLoginAndPassword(@Param("v_email") String username,
+			@Param("v_password") String password);
 }
