82c15b6024dd4c89cd7df38ae0aa84e06008c3eb
Vinicius Escame <v.escame@gmail.com>
2019-10-31 11:00:17 -0300

    added address service

---
+++ src/edu/les/service/AddressService.java
@@ -0,0 +1,65 @@
+package edu.les.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.les.entity.AddressEntity;
+import edu.les.exception.ExceptionHandler;
+import edu.les.repository.AddressRepository;
+
+@Service
+public class AddressService {
+	@Autowired
+	public AddressRepository addressRepository;
+
+	public boolean add(AddressEntity addressEntity) throws ExceptionHandler {
+		boolean result = false;
+		try {
+			this.addressRepository.save(addressEntity);
+			result = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ExceptionHandler();
+		}
+		return result;
+	}
+
+	public boolean hasErrors(AddressEntity a) throws ExceptionHandler {
+		boolean result = true;
+		List<String> errorFields = new ArrayList<String>();
+
+		if (a == null) {
+			return result;
+		}
+
+		if (a.getZipCode().length() != 8) {
+			errorFields.add("Zip Code");
+		}
+
+		if (a.getStreet().length() == 0 || a.getStreet().length() > 45) {
+			errorFields.add("Street");
+		}
+
+		if (a.getDistrict().length() == 0 || a.getDistrict().length() > 45) {
+			errorFields.add("District");
+		}
+
+		if (a.getCity().length() == 0 || a.getCity().length() > 45) {
+			errorFields.add("City");
+		}
+
+		if (a.getFederalUnit().length() == 0 || a.getFederalUnit().length() > 2) {
+			errorFields.add("FederalUnit");
+		}
+
+		if (errorFields.isEmpty()) {
+			result = false;
+		} else {
+			throw new ExceptionHandler();
+		}
+		return result;
+	}
+}
