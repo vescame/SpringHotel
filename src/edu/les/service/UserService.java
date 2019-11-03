4334b63f4ba4354ba979182141b07d37ec12d4b0
Vinicius Escame <v.escame@gmail.com>
2019-11-03 12:12:54 -0300

    implemented birthday validation

--- src/edu/les/service/UserService.java
+++ src/edu/les/service/UserService.java
@@ -1,6 +1,10 @@
 package edu.les.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,17 +26,19 @@ public class UserService {
 	@Autowired
 	private CredentialService credentialService;
 
+	DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
 	public boolean add(UserEntity userEntity) throws ExceptionHandler {
 		boolean result = false;
 		this.userRepository.save(userEntity);
 		result = true;
 		return result;
 	}
-	
+
 	public Optional<UserEntity> findByCpf(String cpf) {
 		return this.userRepository.findById(cpf);
 	}
-	
+
 	public boolean updateUser(UserEntity userEntity) throws ExceptionHandler {
 		boolean result = false;
 		if (hasErrors(userEntity)) {
@@ -41,8 +47,8 @@ public class UserService {
 		result = this.userRepository.save(userEntity) != null;
 		return result;
 	}
-	
-	public Iterable<UserEntity> fetchAll(){
+
+	public Iterable<UserEntity> fetchAll() {
 		return this.userRepository.findAll();
 	}
 
@@ -78,13 +84,15 @@ public class UserService {
 			errorFields.add("Celphone");
 		}
 
-		if (u.getDateOfBirth() == null) {
-			errorFields.add("Birthday");
+		if (u.getDateOfBirth() != null) {
+			if (!this.isBirthdayValid(u.getDateOfBirth())) {
+				errorFields.add("Birthday");
+			}
 		}
 
 		// TODO: check if is needed to validate user status
 		// because it is hardcoded at the register
-
+		
 		try {
 			// catch exception gotten inside those classes
 			// no need to receive boolean results
@@ -93,13 +101,28 @@ public class UserService {
 		} catch (ExceptionHandler h) {
 			errorFields.addAll(h.getErrors());
 		}
-		
+
 		if (errorFields.isEmpty()) {
 			result = false;
 		} else {
 			throw new ExceptionHandler(errorFields);
 		}
-		
+
 		return result;
+	}
+
+	public boolean isBirthdayValid(Date date) {
+		this.sdf.setLenient(false);
+		try {
+			String dateStr = this.sdf.format(date);
+			String[] yMd = dateStr.split("\\-");
+			if (yMd[0].length() > 4) {
+				throw new ParseException("The year has more than 4 chars", 1);
+			}
+			this.sdf.parse(dateStr);
+		} catch (ParseException e) {
+			return false;
+		}
+		return true;
 	}
 }
