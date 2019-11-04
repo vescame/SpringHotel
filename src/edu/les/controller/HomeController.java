fe1e2cab281856ef45af0fa6fd1d363747819c0c
Vinicius Escame <v.escame@gmail.com>
2019-11-04 00:20:27 -0300

    added home controller

---
+++ src/edu/les/controller/HomeController.java
@@ -0,0 +1,29 @@
+package edu.les.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.les.security.SpringHotelSession;
+import edu.les.service.UserService;
+
+@Controller
+public class HomeController {
+	@Autowired
+	UserService userService;
+
+	private final String viewUrl = "/home";
+
+	@RequestMapping(value = viewUrl, method = RequestMethod.GET)
+	public ModelAndView loginView(Model model) {
+		ModelAndView modelAndView = new ModelAndView(this.viewUrl);
+		final String roleDesc = SpringHotelSession.getLoggedInUser().getUserRole().getRoleDescription();
+		if (roleDesc.equals("ADMINISTRATOR") || roleDesc.equals("EMPLOYEE")) {
+			modelAndView.addObject("IS_AUTHORIZED");
+		}
+		return modelAndView;
+	}
+}
