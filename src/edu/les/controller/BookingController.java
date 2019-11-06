4dabfef73ba0f7f67d1e48a49bb6b94e8e33bc5b
Vinicius Escame <v.escame@gmail.com>
2019-11-06 10:36:50 -0300

    booking implementation

--- src/edu/les/controller/BookingController.java
+++ src/edu/les/controller/BookingController.java
@@ -1,34 +1,54 @@
 package edu.les.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.les.entity.BookingEntity;
 import edu.les.entity.RoomEntity;
+import edu.les.exception.ExceptionHandler;
+import edu.les.security.SpringHotelSession;
+import edu.les.service.BookingService;
 //import edu.les.service.BookingService;
 import edu.les.service.RoomService;
 
 @Controller
 public class BookingController {
-//	@Autowired 
-//	private BookingService bookingService;
-	
+	@Autowired 
+	private BookingService bookingService;
+
 	@Autowired
 	private RoomService roomService;
-	
+
 	private final String viewUrl = "booking";
 	private final String viewObjectName = "bookingEntity";
-	
+
 	@RequestMapping(value = viewUrl, method = RequestMethod.GET)
 	public ModelAndView bookingView(Model model) {
 		ModelAndView modelAndView = new ModelAndView(this.viewUrl);
 		modelAndView.addObject(viewObjectName, new BookingEntity());
 		Iterable<RoomEntity> disponible = this.roomService.fetchDisponible();
 		modelAndView.addObject("disponibleRooms", disponible);
+		modelAndView.addObject("loggedUser", SpringHotelSession.getLoggedInUser());
+		return modelAndView;
+	}
+
+	@RequestMapping(value = viewUrl, method = RequestMethod.POST)
+	public ModelAndView bookingRegister(@ModelAttribute(viewObjectName) BookingEntity bookingEntity,
+			HttpServletRequest request, RedirectAttributes redir) {
+		ModelAndView modelAndView = new ModelAndView(this.viewUrl);
+		try {
+			this.bookingService.add(bookingEntity);
+		} catch (ExceptionHandler e) {
+			redir.addFlashAttribute("STATUS_MESSAGE", e.getMessage());
+		}
 		return modelAndView;
 	}
 }
--- src/edu/les/service/BookingService.java
+++ src/edu/les/service/BookingService.java
@@ -1,5 +1,8 @@
 package edu.les.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +13,56 @@ import edu.les.repository.BookingRepository;
 @Service
 public class BookingService{
 	@Autowired
-	public BookingRepository bookingRepository;
+	private BookingRepository bookingRepository;
 	
-	public boolean add(BookingEntity bookingEntity) throws ExceptionHandler{
-		boolean result = false;
-		try {
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RoomService roomService;
+	
+	public void add(BookingEntity bookingEntity) throws ExceptionHandler{
+		if (!this.hasErrors(bookingEntity)) {
 			this.bookingRepository.save(bookingEntity);
-			result = true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new ExceptionHandler();
 		}
+	}
+
+	private boolean hasErrors(BookingEntity b) throws ExceptionHandler {
+		boolean result = true;
+		List<String> errorFields = new ArrayList<>();
+		if (b.getBookingId() <= 0) {
+			errorFields.add("Booking ID");
+		}
+		
+		if (b.getCheckIn() == null) {
+			errorFields.add("Check In Date");
+		}
+		
+		// checkout < checkin : < 0
+		// checkout > checkin : > 0
+		// checkout == checkin : == 0
+		// checkout can be null
+		if (b.getCheckOut() != null && b.getCheckOut().compareTo(b.getCheckIn()) < 0) {
+			errorFields.add("Check Out Date");
+		}
+		
+		if (b.getStatus() == null) {
+			errorFields.add("Status");
+		}
+		
+		try {
+			this.userService.hasErrors(b.getUserEntity());
+			this.roomService.hasErrors(b.getRoomEntity());
+		} catch (ExceptionHandler e) {
+			errorFields.addAll(e.getErrors());
+		}
+		
+		if (errorFields.isEmpty()) {
+			result = false;
+		} else {
+			throw new ExceptionHandler(errorFields);
+		}
+		
 		return result;
 	}
 }
